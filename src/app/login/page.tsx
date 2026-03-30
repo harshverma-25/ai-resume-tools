0a08202c0aa952fee99599800be4acffe97f2ff4
Harsh Verma <hv.harshverma025@gmail.com>
2026-03-30 09:21:27 +0530

    feat : setup the supabase

---
+++ src/app/login/page.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import { useState } from "react"
+import { supabase } from "@/src/lib/supabase"
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSignup = async () => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    })
+
+    if (error) {
+      alert(error.message)
+    } else {
+      alert("Signup successful! Check your email.")
+    }
+  }
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      alert(error.message)
+    } else {
+      alert("Login successful")
+    }
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-[350px] border p-6 rounded-lg">
+
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          Login / Signup
+        </h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full border p-2 mb-3"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full border p-2 mb-4"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          onClick={handleLogin}
+          className="w-full bg-black text-white p-2 mb-2"
+        >
+          Login
+        </button>
+
+        <button
+          onClick={handleSignup}
+          className="w-full border p-2"
+        >
+          Sign Up
+        </button>
+
+      </div>
+    </div>
+  )
+}
